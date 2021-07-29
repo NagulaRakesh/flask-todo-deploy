0d55f09cc19fae8f8280a3367eb986be5ccb5523
NagulaRakesh <rakeshyadav6681@gmail.com>
2021-07-29 20:35:03 +0530

    Modifying database issues2

--- application/__init__.py
+++ application/__init__.py
@@ -7,7 +7,9 @@ from config import Config
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fisjaugiasxyki:a945f138d294dd27a828141f3844484bdb3c615961102ccfe150291f17239452@ec2-52-0-67-144.compute-1.amazonaws.com:5432/df5mj5s2tjgcgn'
+    
     db = SQLAlchemy(app)
 
     from . import db 
