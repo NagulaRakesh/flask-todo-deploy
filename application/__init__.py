c29bfcc131d3dbe401bea06d4453f7c5ca46f869
NagulaRakesh <rakeshyadav6681@gmail.com>
2021-07-29 20:02:27 +0530

    Removes import heroku

--- application/__init__.py
+++ application/__init__.py
@@ -3,13 +3,11 @@ import os
 from flask import Flask, render_template, request
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from flask_heroku import Heroku
 from config import Config
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fisjaugiasxyki:a945f138d294dd27a828141f3844484bdb3c615961102ccfe150291f17239452@ec2-52-0-67-144.compute-1.amazonaws.com:5432/df5mj5s2tjgcgn'
-    heroku = Heroku(app)
     db = SQLAlchemy(app)
 
     from . import db 
