cae048b09870b52c2ef9d7bc23df4b37407cb367
NagulaRakesh <rakeshyadav6681@gmail.com>
2021-07-29 21:10:50 +0530

    Adds database url2

--- application/__init__.py
+++ application/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 
 def create_app():
     app = Flask(__name__)
-    DATABASE_URL = os.environ['DATABASE_URL'] = 'postgres://fisjaugiasxyki:a945f138d294dd27a828141f3844484bdb3c615961102ccfe150291f17239452@ec2-52-0-67-144.compute-1.amazonaws.com:5432/df5mj5s2tjgcgn'
+    DATABASE_URL = os.environ['postgres://fisjaugiasxyki:a945f138d294dd27a828141f3844484bdb3c615961102ccfe150291f17239452@ec2-52-0-67-144.compute-1.amazonaws.com:5432/df5mj5s2tjgcgn']
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     
     from . import db 
