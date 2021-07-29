7b98fff3262c99d5557722f7cf6f2c2419b72bdd
NagulaRakesh <rakeshyadav6681@gmail.com>
2021-07-29 21:42:29 +0530

    Adds database url

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 def create_app():
     app = Flask(__name__)
-    DATABASE_DEFAULT = 'TODO'
+    DATABASE_DEFAULT = 'postgres://fisjaugiasxyki:a945f138d294dd27a828141f3844484bdb3c615961102ccfe150291f17239452@ec2-52-0-67-144.compute-1.amazonaws.com:5432/df5mj5s2tjgcgn'
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', DATABASE_DEFAULT)
     db = SQLAlchemy(app)
     
