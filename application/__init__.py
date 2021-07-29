fba358e8125809ec8e897d6f0debf353f875bd30
NagulaRakesh <rakeshyadav6681@gmail.com>
2021-07-29 20:56:20 +0530

    Modifying database issues3

--- application/__init__.py
+++ application/__init__.py
@@ -1,17 +1,12 @@
 import os
-
+import psycopg2
 from flask import Flask, render_template, request
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-from config import Config
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fisjaugiasxyki:a945f138d294dd27a828141f3844484bdb3c615961102ccfe150291f17239452@ec2-52-0-67-144.compute-1.amazonaws.com:5432/df5mj5s2tjgcgn'
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     
-    db = SQLAlchemy(app)
-
     from . import db 
     db.init_app(app)
 
